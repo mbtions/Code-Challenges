6a44c52e608eb5869b828a7fda67c8d706faa70c
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-21 01:06:21 +0530

    added complete commnts

--- solution-challenge-1.java
+++ solution-challenge-1.java
@@ -2,23 +2,23 @@ class Solution
 {
     public int[] twoSum(int[] numbers, int target) 
     {
-        int indices[];
+        int indices[]; // array to return indexes
         indices = new int [2]; //since we have to only return 2 indexes
          if(numbers.length < 2)
          {
-             return null;
+             return null; // if array has less than 2 numbers then no sum hence no target
          }
         
           for(int i = 0; i < numbers.length; i++)
           {
               for(int j = i+1; j < numbers.length; j++)
               {
-                   if(numbers[i]+numbers[j] == target && i < j)
+                   if(numbers[i]+numbers[j] == target && i < j)  // if index1<index2 condition is also followed along with the getting target
                    {
                         indices[0] = i+1;
                         indices[1] = j+1;
                        
-                       break;
+                       break;   // since we find both the indexes then terminating the loop and returning array of indices
                    }
               }
              
