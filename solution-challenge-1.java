06df08126d0f417eb6247f003e3d7947af5c4d57
Meenakshi Bharadwaj <59952791+minakshi29bharadwaj@users.noreply.github.com>
2020-06-21 01:09:17 +0530

    comments added

--- solution-challenge-1.java
+++ solution-challenge-1.java
@@ -6,14 +6,14 @@ class Solution
         indices = new int [2]; //since we have to only return 2 indexes
          if(numbers.length < 2)
          {
-             return null; // if array has less than 2 numbers then no sum hence no target
+             return null; // if array has less than 2 numbers then no sum hence no target therefore no indexes to return
          }
         
           for(int i = 0; i < numbers.length; i++)
           {
               for(int j = i+1; j < numbers.length; j++)
               {
-                   if(numbers[i]+numbers[j] == target && i < j)  // if index1<index2 condition is also followed along with the getting target
+                   if(numbers[i]+numbers[j] == target && i < j)  // if index1<index2 condition is also followed along with getting the target
                    {
                         indices[0] = i+1;
                         indices[1] = j+1;
